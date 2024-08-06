51b8a4e1a81219ee33ea6328d0edacd65229906e
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2024-08-06 13:25:11 +0530

    updated js code

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var ul=document.getElementById("list-container");
 
+
 function additem()
 {	
 	var listitem=document.createElement("li");
 	listitem.innerHTML=input.value+"<button class='del' onclick='deleteitem(event)'>Delete</button>";
 	ul.append(listitem);
+	input.value="";
 }
 
 function deleteitem(event)
